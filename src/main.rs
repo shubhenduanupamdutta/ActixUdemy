758e68894f417ef97a848f4b4a10d9abd819c7ab
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-24 21:12:43 +0530

    A very basic home route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware::Logger, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Initializing dotenv to load environment variables
+    dotenv::dotenv().ok();
+
+    // Initializing logger for logging
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .route("/", actix_web::web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+async fn index() -> &'static str {
+    "hello world"
 }
